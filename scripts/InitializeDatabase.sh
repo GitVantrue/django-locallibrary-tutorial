79e06d9db1a9191cea433457b0e573bf7e07b886
root <root@ip-10-0-120-55.ap-northeast-2.compute.internal>
2022-04-14 06:58:57 +0000

    upload scripts files and settings

--- scripts/InitializeDatabase.sh
+++ scripts/InitializeDatabase.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat << EOF | PGPASSWORD=I,4JRO4SCaSK8GJBZ0CHERUCgvpveM psql -h locallibrary-dev.c6pkffcrrwx8.ap-northeast-2.rds.amazonaws.com -U postgres
+cat << EOF | PGPASSWORD=Orw5F_BMv3NeDV8NPw-z5_fbNmfpZe psql -h locallibrary-prod.c6pkffcrrwx8.ap-northeast-2.rds.amazonaws.com -U postgres
 CREATE DATABASE IF NOT EXISTS local_library;
 CREATE IF NOT EXISTS USER local_library WITH PASSWORD 'asdf1234';
 GRANT ALL PRIVILEGES ON DATABASE local_library TO local_library;
