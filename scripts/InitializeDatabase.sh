e12da20c30ce216fd8ca0ba2eb543efe3ba5e37f
root <root@ip-10-0-120-55.ap-northeast-2.compute.internal>
2022-04-14 06:48:38 +0000

    upload scripts files

--- scripts/InitializeDatabase.sh
+++ scripts/InitializeDatabase.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-cat << EOF | psql -h localhost -U postres
-CREATE DATABASElocal_library;
-CREATE USER local_library WITH PASSWORD 'asdf1234';
+cat << EOF | PGPASSWORD=I,4JRO4SCaSK8GJBZ0CHERUCgvpveM psql -h locallibrary-dev.c6pkffcrrwx8.ap-northeast-2.rds.amazonaws.com -U postgres
+CREATE DATABASE IF NOT EXISTS local_library;
+CREATE IF NOT EXISTS USER local_library WITH PASSWORD 'asdf1234';
 GRANT ALL PRIVILEGES ON DATABASE local_library TO local_library;
 ALTER USER local_library CREATEDB;
 EOF
